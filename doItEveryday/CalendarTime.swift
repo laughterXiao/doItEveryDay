40645d38e948bbf304f0df4eed6a1da00dda2985
haha <gg8tw@yahoo.com.tw>
2019-06-09 17:15:19 +0800

    simple calendar

---
+++ doItEveryday/CalendarTime.swift
@@ -0,0 +1,68 @@
+//
+//  nowTime.swift
+//  doItEveryday
+//
+//  Created by joe on 2019/6/8.
+//  Copyright © 2019 laughterXiao. All rights reserved.
+//
+
+import Foundation
+
+class CalendarTime {
+    var year = 2019
+    var month = 1
+    var day = 1
+    
+    init(){
+        let calendar = Calendar.current
+        let com = calendar.dateComponents([.year,.month,.day], from: Date())
+        year = com.year!
+        month = com.month!
+        day = com.day!
+//        month=11
+    }
+
+    func moveMonth(delta:Int){
+        day = 10
+        month = month+delta
+        if(month>12){
+            month=1
+            year = year+1
+        }else if(month<1){
+            month=12
+            year = year-1
+        }
+        
+    }
+    func getNowMonthDaysCount() ->Int {
+        let calendar = Calendar(identifier:Calendar.Identifier.gregorian)
+        let range = (calendar as NSCalendar?)?.range(of: NSCalendar.Unit.day, in: NSCalendar.Unit.month, for: getNowTimeDate())
+        
+        return (range?.length)!
+    }
+    
+    func getMonthFirstWeekDay() ->Int {
+        let calendar = Calendar(identifier:Calendar.Identifier.gregorian)
+        var firstWeekDay = (calendar as NSCalendar?)?.ordinality(of: NSCalendar.Unit.weekday, in: NSCalendar.Unit.weekOfMonth, for: getNowTimeDate())
+        firstWeekDay = firstWeekDay!-1
+//        if firstWeekDay==0 {
+//            firstWeekDay=7
+//        }
+        print(firstWeekDay)
+        return firstWeekDay!
+    }
+    
+    func getNowTimeDate(dateFormat:String="yyyy-MM-dd") -> Date{
+        let dateFormatter = DateFormatter.init()
+        dateFormatter.dateFormat = dateFormat
+        let timeString:String = String(year)+"-"+String(month)+"-1"
+        let date = dateFormatter.date(from: timeString)!
+        print(timeString)
+        return date
+    }
+    
+    func getMonthYearName() ->String {
+        var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+        return months[month-1]+" "+String(year)
+    }
+}
--- doItEveryday/ViewController.swift
+++ doItEveryday/ViewController.swift
@@ -8,13 +8,73 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UICollectionViewDataSource,UICollectionViewDelegate,UICollectionViewDelegateFlowLayout {
+    let calendarTime:CalendarTime = CalendarTime.init()
+    @IBOutlet weak var calendarView: UICollectionView!
+    @IBOutlet weak var monthLabel: UILabel!
+    
+    @IBAction func nextMonth(_ sender: Any) {
+        calendarTime.moveMonth(delta: 1)
+        calendarView.reloadData()
+        monthLabel.text = calendarTime.getMonthYearName()
+    }
+    
+    @IBAction func preMonth(_ sender: Any) {
+        calendarTime.moveMonth(delta: -1)
+        calendarView.reloadData()
+        monthLabel.text = calendarTime.getMonthYearName()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return calendarTime.getNowMonthDaysCount()+calendarTime.getMonthFirstWeekDay()
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        calendarView.collectionViewLayout.invalidateLayout()
+    }
+    
+    //每格的內容
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        if let textLabel = cell.contentView.subviews[0] as? UILabel{
+            let monthFirstDay = calendarTime.getMonthFirstWeekDay()
+            if indexPath.row+1-monthFirstDay>0 {
+                textLabel.text = String(indexPath.row+1-monthFirstDay)
+            }else{
+                textLabel.text = ""
+            }
+        }
+        return cell
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    //間距調整
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+//        return 0
+//    }
+    
+    //每格的寬高
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = collectionView.frame.width / 7
+        return CGSize(width: width, height: 40)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+//        calendarView.dataSource = self
+//        calendarView.delegate = self
     }
-
+    
+    
 
 }
 
